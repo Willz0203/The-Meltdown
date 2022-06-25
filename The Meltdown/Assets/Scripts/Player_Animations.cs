ba97a7235afe4e3c0d2e52a5d9d46325287dec24
JozNski <108107473+JozNski@users.noreply.github.com>
2022-06-25 15:28:49 -0400

    more pngs/updated rightwalk

--- The Meltdown/Assets/Scripts/Player_Animations.cs
+++ The Meltdown/Assets/Scripts/Player_Animations.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCombat : MonoBehaviour
+public class Player_Animations : MonoBehaviour
 {
    public Animator animator;
 
